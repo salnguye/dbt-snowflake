02ed6631e09710698dbbfe4c98777b9b426beba4
Sally Nguyen <salnguyen95@gmail.com>
2023-10-04 21:18:01 +0000

    dbt packages and macros demo

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,6 +4,6 @@ select
     paymentmethod as payment_method,
     status,
     -- Converting amounts in cents into dollars
-    amount/100 as amount,
+    {{ cent_to_dollars('amount') }} as amount,
     created as created_at
 from {{ source('stripe', 'payment') }}
